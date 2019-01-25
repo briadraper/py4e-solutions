523632310de4c049c7e43745d9e05597dea54e5c
briadraper <41592974+briadraper@users.noreply.github.com>
2019-01-25 11:08:30 -0800

    Create exercise_02.02.py

---
+++ exercise_02.02.py
@@ -0,0 +1,12 @@
+Exercise 2: Write a program that uses input to prompt a user for their name and the welcomes them.
+
+Enter your name: Chuck
+Hello Chuck
+
+Python for Everybody: Exploring Data Using Python 3
+By Charles Severance
+
+-------------
+
+name = input("Enter your name: ")
+print('Hello ' + name)
