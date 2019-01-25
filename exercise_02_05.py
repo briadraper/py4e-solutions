4c4a9612e5fe536f63562721a69df29892e002da
briadraper <41592974+briadraper@users.noreply.github.com>
2019-01-25 11:25:58 -0800

    Create exercise_02_05.py

---
+++ exercise_02_05.py
@@ -0,0 +1,11 @@
+Exercise 5: Write a program that prompts the user for a Celsius temperature, convert the temperature to Fahranheit, and print out the converted temperature.
+
+Python for Everybody: Exploring Data in Python 3
+
+By Charles Severance
+
+....
+
+Cels = input('Enter in a temperature in degrees Celsius: ')
+Far = float(Cels)*9/5+32
+print('Fahrenheit:', Far)
