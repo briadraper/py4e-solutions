b364f8a0de9f7332190d2eebebaa4e0de34ff09f
briadraper <41592974+briadraper@users.noreply.github.com>
2019-02-06 10:36:05 -0800

    Update exercise_02_02.py

--- exercise_02_02.py
+++ exercise_02_02.py
@@ -1,4 +1,4 @@
-Exercise 2: Write a program that uses input to prompt a user for their name and the welcomes them.
+Exercise 2: Write a program that uses input to prompt a user for their name and then welcomes them.
 
 Enter your name: Chuck
 Hello Chuck
