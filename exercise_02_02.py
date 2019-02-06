888aa0464dfa9b497e7ec483f75be97b85fbef56
briadraper <41592974+briadraper@users.noreply.github.com>
2019-02-06 11:22:31 -0800

    Update exercise_02_02.py

--- exercise_02_02.py
+++ exercise_02_02.py
@@ -1,10 +1,8 @@
-Exercise 2: Write a program that uses input to prompt a user for their name and then welcomes them.
-
+“Exercise 2   Write a program that uses raw_input to prompt a user for their name and then welcomes them.
 Enter your name: Chuck
-Hello Chuck
+Hello Chuck”
 
-Python for Everybody: Exploring Data Using Python 3
-By Charles Severance
+Excerpt From: Charles R. Severance. “Python for Informatics: Exploring Information.” Apple Books. 
 
 ....
 
