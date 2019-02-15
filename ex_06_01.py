bc23ff035f31ee6a3edee9eb2e6105d13b456411
briadraper <41592974+briadraper@users.noreply.github.com>
2019-02-15 11:19:58 -0800

    Create ex_06_01.py

---
+++ ex_06_01.py
@@ -0,0 +1,26 @@
+“Exercise 1   Write a while loop that starts at the last character in the string and works its way backwards to the first character in the string, printing each letter on a separate line, except backwards.”
+
+Excerpt From: Charles R. Severance. “Python for Informatics: Exploring Information.” Apple Books. 
+
+....
+
+bird = 'pigeon'
+len(bird)
+length = len(bird)
+index = -1
+while index < len(bird) :
+    letter = bird[index]
+    print(length)
+    last = bird[-1]
+    print(last)
+    second = bird[-2]
+    print(second)
+    third = bird[-3]
+    print(third)
+    fourth = bird[-4]
+    print(fourth)
+    fifth = bird[-5]
+    print(fifth)
+    sixth = bird[-6]
+    print(sixth)
+    break
