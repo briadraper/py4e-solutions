d1bec826cf986ec3b903d2d899930f8786e49547
briadraper <41592974+briadraper@users.noreply.github.com>
2019-02-15 10:35:14 -0800

    Update exercise_05_02.py

--- exercise_05_02.py
+++ exercise_05_02.py
@@ -20,5 +20,5 @@ while True :
     except :
             print('Invalid input')
 
-print ('Maximum is', largest)
-print ('Minimum is', smallest)
+print ('Maximum is ', largest)
+print ('Minimum is ', smallest)
